8006e60bbf90910e817806a9f073f725bec3b5d5
Chandraket Yadav <chandraketyadav505@gmail.com>
2022-08-29 06:53:12 +0530

    Update word_cloud.py

--- word_cloud.py
+++ word_cloud.py
@@ -1,19 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# # Final Project - Word Cloud
-
-# For this project, you'll create a "word cloud" from a text by writing a script.  This script needs to process the text, remove punctuation, ignore case and words that do not contain all alphabets, count the frequencies, and ignore uninteresting or irrelevant words.  A dictionary is the output of the `calculate_frequencies` function.  The `wordcloud` module will then generate the image from your dictionary.
-
-# For the input text of your script, you will need to provide a file that contains text only.  For the text itself, you can copy and paste the contents of a website you like.  Or you can use a site like [Project Gutenberg](https://www.gutenberg.org/) to find books that are available online.  You could see what word clouds you can get from famous books, like a Shakespeare play or a novel by Jane Austen. Save this as a .txt file somewhere on your computer.
-# <br><br>
-# Now you will need to upload your input file here so that your script will be able to process it.  To do the upload, you will need an uploader widget.  Run the following cell to perform all the installs and imports for your word cloud script and uploader widget.  It may take a minute for all of this to run and there will be a lot of output messages. But, be patient. Once you get the following final line of output, the code is done executing. Then you can continue on with the rest of the instructions for this notebook.
-# <br><br>
-# **Enabling notebook extension fileupload/extension...**
-# <br>
-# **- Validating: <font color =green>OK</font>**
-
-# In[14]:
 
 
 
@@ -133,7 +118,6 @@ plt.show()
 
 # If your word cloud image did not appear, go back and rework your `calculate_frequencies` function until you get the desired output.  Definitely check that you passed your frequecy count dictionary into the `generate_from_frequencies` function of `wordcloud`. Once you have correctly displayed your word cloud image, you are all done with this project. Nice work!
 
-# In[ ]:
 
 
 
